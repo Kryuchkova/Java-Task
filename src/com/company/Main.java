614bf8ef7d7f20e41d47c931220ead42f365099a
Alexandra <skripka23@mail.ru>
2019-08-18 21:52:04 +0400

    Arithmetic Operators Module: 2st task - Write a program that which takes an year "N" as input and prints true if N corresponds to a leap year, and false otherwise. Assumes N >= 1582, corresponding to a year in the Gregorian calendar.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    Scanner s = new Scanner(System.in);
-	    System.out.print("Enter value a: ");
-	    int a = s.nextInt();
-        System.out.print("Enter value b: ");
-        int b = s.nextInt();
-        System.out.print("The result of adding is ");
-        System.out.println(a + b);
-        System.out.print("The result of subtracting is ");
-        System.out.println(a - b);
-        System.out.print("The result of multiplying is ");
-        System.out.println(a * b);
-        System.out.print("The result of dividing is ");
-        System.out.println((float) a / b);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the year to be checked: ");
+        int year = scanner.nextInt();
+        boolean isLeapYear;
+
+        if (year % 400 == 0 || year % 100 != 0 && year % 4 == 0)
+        {
+            isLeapYear = true;
+        }
+        else
+        {
+            isLeapYear = false;
+        }
+        System.out.println(isLeapYear);
     }
 }
