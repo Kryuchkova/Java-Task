5d9f1fd5b1eb1b96d6a989cae336989fe8e0bf89
Alexandra <skripka23@mail.ru>
2019-08-18 21:31:53 +0400

    Arithmetic Operators Module: 1st task - Write Java program to allow the user to input two integer values and then the program prints the results of adding, subtracting, multiplying, and dividing among the two values.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello!");
+	    Scanner s = new Scanner(System.in);
+	    System.out.print("Enter value a: ");
+	    int a = s.nextInt();
+        System.out.print("Enter value b: ");
+        int b = s.nextInt();
+        System.out.print("The result of adding is ");
+        System.out.println(a + b);
+        System.out.print("The result of subtracting is ");
+        System.out.println(a - b);
+        System.out.print("The result of multiplying is ");
+        System.out.println(a * b);
+        System.out.print("The result of dividing is ");
+        System.out.println((float)(a / b));
     }
 }
